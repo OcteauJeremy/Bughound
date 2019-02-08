afcd4f7b17cdbbaed4ac7ed28b4dd2d2ce48ff4d
SHagege <solalhagege@gmail.com>
2019-02-07 18:32:30 -0800

    Add Employee page

--- bughound_webapp/src/app/app.component.ts
+++ bughound_webapp/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bughound';
+  employee=[];
+
+  submit(){
+    this.employee.push()
+    this.employee.toString()
+  }
 }
