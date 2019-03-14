4c2d9dabadf3eb35dfa6589311c7351772d41531
SHagege <solalhagege@gmail.com>
2019-03-14 01:09:13 -0700

    Add button to delete bug

--- bughound_webapp/src/app/bugs/bugs-list/bugs-list.component.ts
+++ bughound_webapp/src/app/bugs/bugs-list/bugs-list.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material';
 import { Router } from '@angular/router';
 import { BugService } from '../../services/bug.service';
 import { getChoiceFromValue } from '../bugs-utils';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-bugs-list',
@@ -19,7 +21,7 @@ export class BugsListComponent implements OnInit {
 
     getName = getChoiceFromValue;
 
-    constructor(private router: Router, private bugService: BugService) { }
+    constructor(private router: Router, private bugService: BugService, public dialog: MatDialog, private toastr: ToastrService) { }
 
     ngOnInit() {
         this.loadBugs()
@@ -34,4 +36,40 @@ export class BugsListComponent implements OnInit {
             this.bugs = res;
         });
     }
+
+    openDialogDeleteBug(bug): void {
+        const dialogRef = this.dialog.open(DialogConfirmDeleteBug, {
+            width: '550px',
+            data: {bug: bug}
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.bugService.deleteBug(bug).subscribe(res => {
+                    this.params_page = {
+                        page_size: 10,
+                        page: 1
+                    };
+                    this.loadBugs();
+                    this.toastr.success('Bug ' + result.name + ' deleted.');
+                });
+            }
+        });
+    }
+}
+
+@Component({
+    selector: 'app-dialog-confirm-delete-bug',
+    templateUrl: 'dialog-confirm-delete-bug.html',
+})
+export class DialogConfirmDeleteBug {
+
+    constructor(
+        public dialogRef: MatDialogRef<DialogConfirmDeleteBug>,
+        @Inject(MAT_DIALOG_DATA) public data) {}
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+
 }
